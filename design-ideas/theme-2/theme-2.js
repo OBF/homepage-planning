1d9ee98d7884ff2972cf762dba01f8525466350e
Deepashree Deshmukh <18piscean@gmail.com>
2018-11-06 00:04:34 +0530

    Updated

--- design-ideas/theme-2/theme-2.js
+++ design-ideas/theme-2/theme-2.js
@@ -1,6 +1,4 @@
-/*$(function() {
-    $('body').removeClass('fade-out'); 
-}); */
+
 
 $(document).ready(function() {
     $('body').fadeIn(500);
@@ -13,6 +11,10 @@ $(document).ready(function() {
         if ($(this).scrollTop() > 850) {    
             $('.fellowships-section').css({'opacity':'1'}); 
             $('.fellowships-background-image').css({'transform':'translateX(-100%)'});
+        }
+        
+        if ($(this).scrollTop() > 1500) {    
+            $('.projects-section').css({'opacity':'1'}); 
         }
     });
 });
